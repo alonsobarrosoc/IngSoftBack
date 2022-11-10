5213d8b84525c8d1b6879da5e23d072fb8a2127f
Alonso Barroso <alonsobarrosoc@gmail.com>
2022-11-10 12:56:39 -0600

    deploy-in-heroku

--- Controllers/asesoriasController.js
+++ Controllers/asesoriasController.js
@@ -5,6 +5,10 @@ const prisma = new PrismaClient()
 
 exports.getAsesoria = async (req, res) => {
     const body = req.body
-    const as = await prisma.asesoria.findMany({})
+    const as = await prisma.asesoria.findMany({
+        where:{
+            1:1
+        }
+    })
     res.status(200).json(as)
 }
