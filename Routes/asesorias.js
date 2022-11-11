ce3c11bd7fc8c69262e6129b106da51326bb9f76
Alonso Barroso <alonsobarrosoc@gmail.com>
2022-11-10 19:27:49 -0600

    deploy-in-heroku

--- Routes/asesorias.js
+++ Routes/asesorias.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const { getAsesoria } = require('../Controllers/asesoriasController')
+const { getAsesoria, deleteAsesoria, putAsesoria, newAsesoria } = require('../Controllers/asesoriasController')
 const router = express.Router()
 
 router.post('/get', getAsesoria)
+router.delete('/delete', deleteAsesoria)
+router.put('/update', putAsesoria)
+router.post('/add', newAsesoria)
 
 module.exports = router
